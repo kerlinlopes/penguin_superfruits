8575c5a2b9b2c63826715c53167da9402cc60386
Kerlin <lopeskerlin@gmail.com>
2021-10-25 14:05:29 -0400

    first commit

---
+++ scratch.js
@@ -0,0 +1,11 @@
+///////////////////////
+//FOR REFERENCE
+///////////////////////
+app.use(express.static("public"))
+// "/styles.css" => "localhost:4000/styles.css"
+
+app.use("/public", express.static("public"))
+// "/public/styles.css" => "localhost:4000/public/styles.css"
+
+app.use("/static", express.static("public"))
+// "/static/styles.css" => "localhost:4000/static/styles.css"
